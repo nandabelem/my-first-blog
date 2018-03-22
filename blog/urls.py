e56aa5ebdef07eec74816564553651c8c999ef5d
Fernanda <fernandambelem@gmail.com>
2018-03-22 16:03:11 -0300

    atualizando local > git

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from .forms import PostForm
 
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
